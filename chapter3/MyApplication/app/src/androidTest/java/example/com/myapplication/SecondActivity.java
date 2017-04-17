60f4176517322ae45f5ded0330e5fc8d01e7a412
AnyAdapter <2641047194@qq.com>
2017-04-17 21:43:01 +0800

    first commit

---
+++ chapter3/MyApplication/app/src/androidTest/java/example/com/myapplication/SecondActivity.java
@@ -0,0 +1,11 @@
+package example.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 何为方向 on 2017/3/16.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+}
